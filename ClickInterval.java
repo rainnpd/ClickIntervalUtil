648c833beb4dd7a527ffd5469381b84fcef9d15a
npd <43715682+rainnpd@users.noreply.github.com>
2019-10-23 10:17:33 +0800

    Add files via upload

---
+++ ClickInterval.java
@@ -0,0 +1,26 @@
+package com.item.video.utils;
+
+/**
+ * 点击间隔
+ */
+public class ClickInterval {
+    // 两次点击按钮之间的点击间隔不能少于1000毫秒
+    private static final int MIN_CLICK_DELAY_TIME = 1000;
+    private static long lastClickTime;
+    private static String contextName;
+
+    public static boolean isFastClick(String context) {
+        long curClickTime = System.currentTimeMillis();
+        if (!context.equals(contextName)) { //不同activity不共用计时
+            contextName = context;
+            lastClickTime = curClickTime;
+            return false;
+        }
+
+        if ((curClickTime - lastClickTime) >= MIN_CLICK_DELAY_TIME) {
+            lastClickTime = curClickTime;
+            return false;
+        }
+        return true;
+    }
+}
